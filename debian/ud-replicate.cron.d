2b578930a239d3a7d19c6b6947dee53b409422c7
joey <>
2004-11-18 16:58:34 +0000

    Removed superflous comment, should force a re-install as well

--- debian/ud-replicate.cron.d
+++ debian/ud-replicate.cron.d
@@ -1,2 +1,2 @@
-# crontab for the replicate operation, 15 min cycle, offset from generate.
+# crontab for the replicate operation, 15 min cycle.
 10,25,40,55 * * * * root if [ -x /usr/bin/ud-replicate ]; then /usr/bin/ud-replicate; fi
