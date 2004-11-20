6cbc7fa975621561fa281bdc552843192b142d59
joey <>
2004-11-20 18:52:02 +0000

    Added a proper copyright notice

--- userdir_ldap.py
+++ userdir_ldap.py
@@ -1,3 +1,21 @@
+#   Copyright (c) 1999-2000  Jason Gunthorpe <jgg@debian.org>
+#   Copyright (c) 2001-2003  Ryan Murray <rmurray@debian.org>
+#   Copyright (c) 2004  Joey Schulze <joey@infodrom.org>
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
 # Some routines and configuration that are used by the ldap progams
 import termios, re, string, imp, ldap, sys, whrandom, crypt, rfc822;
 import userdir_gpg
